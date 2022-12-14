f49868ae3ac486af624484a133c9d97a221d780d
Sergey Sochinskiy <empty.headed21@gmail.com>
2022-12-14 11:28:06 +0300

    Added exit game button

--- Assets/Scripts/Ui/UiButtonsManager.cs
+++ Assets/Scripts/Ui/UiButtonsManager.cs
@@ -5,4 +5,5 @@ public class UiButtonsManager : MonoBehaviour
     public void StartGameButton() => EventHandler.StartGameEvent.Invoke();
     public void ReturnMainMenuButton() => EventHandler.ReturnMainMenuEvent.Invoke();
     public void RestartLevel() => EventHandler.RestartLevelEvent.Invoke();
+    public void ExitGame() => Application.Quit();
 }
