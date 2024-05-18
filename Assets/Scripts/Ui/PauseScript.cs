8d63c6112d985f88f13f0fa5b88321dd48f0f548
Sergey S <empty.headed21@gmail.com>
2024-05-18 16:58:56 +0300

    pause bugs and ads bugs fix

--- Assets/Scripts/Ui/PauseScript.cs
+++ Assets/Scripts/Ui/PauseScript.cs
@@ -1,12 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using YG;
 
 public class PauseScript : MonoBehaviour
 {
+    private bool _canWork;
+
+    private void Awake()
+    {
+        _canWork = true;
+        YandexGame.OpenFullAdEvent += () => _canWork = false;
+        YandexGame.CloseFullAdEvent += () => _canWork = true;
+    }
+
     private void OnApplicationFocus(bool focus)
     {
-        if(focus)
+        if(_canWork)
+            Enable(focus);
+    }
+
+    public void Enable(bool isActive)
+    {
+        if(isActive)
         {
             Time.timeScale = 1;
             AudioListener.pause = false;
